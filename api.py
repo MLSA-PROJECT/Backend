3a27e00d49a6cecf1cba27476a68bd5508874ced
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-08-18 20:23:40 +0530

    diabetes completed

--- api.py
+++ api.py
@@ -1,40 +1,50 @@
 import joblib
 from flask import Flask
 from flask import request
-
+import json
 app = Flask(__name__)
 
 # loading the models
 diabetes_model = joblib.load("diabetes_model.sav")
-heart_model = joblib.load("heart_model.sav")
+heart_model = joblib.load("heart_disease_model.sav")
 parkinsons_model = joblib.load("parkinsons_model.sav")
 
 @app.route("/api/diabetes", methods=['GET', 'POST'])
 def diabetes_predict():
     data = request.data
 
-    # input parameters
-    # number of pregnancies
-    # glucose level
-    # blood pressure value
-    # skin thickness value
-    # Insulin level
-    # BMI Value
-    # diabetes pedigree function value
-    # age
+    # parse x:
+    data = json.loads(data)
 
-    res = diabetes_model.predict()
+    # input parameters
+    pregenancies = data["pregenancies"]
+    glucose = data["glucose"]
+    blood_pressure = data["blood_pressure"]
+    skin_thickness = data["skin_thickness"]
+    insulin = data["insulin"]
+    bmi = data["bmi"]
+    diabetes_pedigree_function = data["diabetes_pedigree_function"]
+    age = data["age"]
+
+    # getting the prediction from the model
+    prediction = diabetes_model.predict([[pregenancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
+
+    if prediction[0] == 0:
+        res = "No"
+    else:
+        res = "Yes"
+
     return res
 
 @app.route("/api/heart", methods=['GET', 'POST'])
-def diabetes_predict():
+def heart_predict():
     # input parameters
 
     res = heart_model.predict()
     return res
 
 @app.route("/api/parkinsons", methods=['GET', 'POST'])
-def diabetes_predict():
+def parkinsons_predict():
     # input parameters
 
     res = parkinsons_model.predict()
@@ -43,7 +53,6 @@ def diabetes_predict():
 @app.route("/api")
 def predict():
     """Call the model and get the desired output"""
-    # res = test()
     res = "Hello world"
     return res
 
